6607379f2093cbd39d38cb4ed4e963d504260a21
Patel Aman <ap9.vit@gmail.com>
2025-12-24 00:25:29 +0530

    Update notebooks

--- pyttsx.py
+++ pyttsx.py
@@ -1,7 +1,7 @@
-# import pyttsx3
-# engine = pyttsx3.init()
-# engine.say("I will speak this text")
-# engine.runAndWait()
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
 
 import pyjokes
 print(pyjokes.get_joke())
